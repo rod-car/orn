4069c2dc284669c933e286cd64549d9e12032efe
rod-car <ghislaincarino@gmail.com>
2024-04-15 08:01:06 +0200

    Mise a jour de la chart

--- packages/functions/index.ts
+++ packages/functions/index.ts
@@ -1,4 +1,4 @@
 export { scholar_years, getAgeYear as ageYear, getAgeMonth as ageMonth, ageLong as ageFull } from "./date";
 export { capitalize, gender } from "./string";
 export { number_array, range, round, isNumber } from "./number";
-export { addDays, subDays } from 'date-fns'
+export { addDays, subDays, isDate } from 'date-fns'
